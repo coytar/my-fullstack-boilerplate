51f393d17251bfb00f45a979e9b09210f0cb09ee
coytar <coytar@outlook.com>
2024-05-06 18:34:54 +1000

    Remove bcrypt because I was trying something.

--- client/src/hooks/useSignup.jsx
+++ client/src/hooks/useSignup.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import useAuthContext from "./useAuthContext";
 import axios from "axios";
-import bcrypt from "bcryptjs";
 
 const useSignup = () => {
   const [error, setError] = useState(null);
